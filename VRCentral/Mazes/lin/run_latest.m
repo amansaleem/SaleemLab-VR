89e9e525fe427d80a3fb12a9aabd4db8bb0a3ee9
tomasaso <muzzutomaso@gmail.com>
2018-03-06 15:47:52 +0000

    g

--- VRCentral/Mazes/lin/run_latest.m
+++ VRCentral/Mazes/lin/run_latest.m
@@ -879,7 +879,7 @@ end
         
         runInfo.List1 = glGenLists(1);
         
-        WLength = 1.0;
+        WLength = 0.25;
         runInfo.List2 = glGenLists(1);
         
         glNewList(runInfo.List1,GL.COMPILE);
