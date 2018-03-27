6d1f6cdfd938f46f62cba1b228a02e4c87868a1f
Aman <aman.saleem@gmail.com>
2018-03-27 11:01:47 +0100

    few upadtes on creating the GUI

--- Behaviour analysis/DevBehavPlot/behavPlotter.m
+++ Behaviour analysis/DevBehavPlot/behavPlotter.m
@@ -3,7 +3,7 @@ classdef behavPlotter < handle
     properties
         expObject
         
-        figHand
+        figHandle
         es
         esLoaded = false;
         totalReward = [];
@@ -13,6 +13,37 @@ classdef behavPlotter < handle
     
     methods
         function createUI(obj)
+            obj.figHandle.MainFig = figure('Name', 'Behaviour PLotter',...
+                'Toolbar', 'none',...
+                'NumberTitle', 'off',...
+                'Units', 'normalized',...
+                'OuterPosition', [0.1 0.1 0.5 0.7]);%...
+            
+            obj.figHandle.Full = uiextras.VBox('Parent',obj.figHandle.MainFig,'Spacing',10,'Padding',5);
+            
+            %Splitting to top and bottom sections
+            obj.figHandle.Top = uiextras.HBox('Parent',obj.figHandle.Full,'Spacing',10,'Padding',5);
+            obj.figHandle.Middle = uiextras.HBox('Parent',obj.figHandle.Full,'Spacing',10,'Padding',5);
+            obj.figHandle.Full.Sizes = [-1 -1];
+            
+            %Spliting the top
+            obj.figHandle.animalInfoPanel = uiextras.VBox('Parent',obj.figHandle.Top,'Spacing',10,'Padding',5);
+            obj.figHandle.PositionTimePlot = axes('Parent',obj.figHandle.Top);
+            obj.figHandle.Top.Sizes = [-1 -5];
+            
+            % Splitting the bottom
+            obj.figHandle.detailsPanel  = uiextras.VBox('Parent',obj.figHandle.Middle,'Spacing',10,'Padding',5);
+            obj.figHandle.HistPosPlot   = axes('Parent',obj.figHandle.Middle);
+            obj.figHandle.VRPosPlot     = axes('Parent',obj.figHandle.Middle);
+            obj.figHandle.SpdProfilePlot = axes('Parent',obj.figHandle.Middle);
+            obj.figHandle.HistSpdPlot   = axes('Parent',obj.figHandle.Middle);
+            obj.figHandle.Middle.Sizes  = [-1 -1 -1 -1 -1];
+            
+            % Animal Info Panel
+            obj.figHandle.animalText = uicontrol('Style','text',...
+                'Parent',obj.figHandle.animalInfoPanel,...
+                'String', obj.expObject.animal,...
+                'fontsize',14, 'HorizontalAlignment','left');
         end
         
         function load(obj)
@@ -40,3 +71,4 @@ classdef behavPlotter < handle
             obj.createUI;
         end
     end
+end
