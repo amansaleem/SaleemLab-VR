4120dff56ac744b6e7a1a88c74432142d7999130
Aman <aman.saleem@gmail.com>
2019-04-17 10:54:01 +0100

    Create copyToNewServer.m

---
+++ Behaviour analysis/copyToNewServer.m
@@ -0,0 +1,17 @@
+function copyToNewServer(animal_name)
+% copyToNewServer(animal_name)
+% Run this for all the animals that we want to transfer the VR behaviour
+% data over to the new Server.
+
+oldDir = 'X:\ibn-vision\Archive - saleemlab\Data\Behav';
+newDir = 'X:\ibn-vision\DATA\SUBJECTS';
+
+oldFullDir = fullfile(oldDir, animal_name);
+
+newAnimalDir = fullfile(newDir, animal_name);
+if ~exist(newAnimalDir); mkdir(newAnimalDir); end
+
+newFullDir = fullfile(newAnimalDir, 'VRBehaviour');
+if ~exist(newFullDir); mkdir(newFullDir); end
+
+[SUCCESS,MESSAGE,MESSAGEID] = copyfile(oldFullDir, newFullDir)
